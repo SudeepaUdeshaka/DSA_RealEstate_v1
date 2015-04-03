d4c6063f77318da15513c97475e99d2c66985a0b
Ishani <IshaniSachi@users.noreply.github.com>
2015-04-03 21:30:14 +0530

    Update SortedList.java

--- src/org/RealEstate/SortedList.java
+++ src/org/RealEstate/SortedList.java
@@ -1,12 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-package org.RealEstate;
+package org.RealEstatev01;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SortedList {
-    
+
+public void Listable()
+    {
+
+ String line = "";
+         BufferedReader r = null;
+        try {
+            r = new BufferedReader(new FileReader("DataFile.txt"));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(SortedList.class.getName()).log(Level.SEVERE, null, ex);
+        }ArrayList al = new ArrayList();
+        try {
+            while ((line = r.readLine()) != null)
+                
+                al.add(line);
+        } catch (IOException ex) {
+            Logger.getLogger(SortedList.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            r.close();
+        } catch (IOException ex) {
+            Logger.getLogger(SortedList.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Collections.sort(al);
+        for (int i=0; i<al.size(); i++)
+        {
+            System.out.println(al.get(i));
+            try(PrintWriter output = new PrintWriter(new FileWriter("DataFileSorted.txt",true))) {
+                            output.printf("%s\r\n",al.get(i));
+                        } 
+                        catch (Exception e) {}
+        }
+            
+        
+    }
 }
